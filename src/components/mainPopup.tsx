ccfface966f88c4822dbd0f83d2b46e98bbbed28
Yasser <yasserfedsi34@gmail.com>
2025-03-26 13:24:19 +0100

    Enhance Popup component: add escape key functionality and improve visibility handling

--- src/components/mainPopup.tsx
+++ src/components/mainPopup.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import PopupHeader from "./Popup/header";
 import PopupContent from "./Popup/content";
@@ -11,27 +11,51 @@ interface PopupProps {
 
 export default function Popup({ isOpen, onClose }: PopupProps) {
   const popupRef = useRef(null);
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     if (isOpen) {
+      setVisible(true);
       document.body.style.overflow = "hidden";
-      gsap.fromTo(
-        popupRef.current,
-        { y: "100%", opacity: 0 },
-        { y: "0%", opacity: 1, duration: 0.8, ease: "power3.out" }
-      );
-    } else {
+
+      setTimeout(() => {
+        gsap.fromTo(
+          popupRef.current,
+          { y: "100%", opacity: 0 },
+          { y: "0%", opacity: 1, duration: 0.8, ease: "power3.out" }
+        );
+      }, 10);
+    } else if (visible) {
       document.body.style.overflow = "auto";
+
       gsap.to(popupRef.current, {
         y: "100%",
         opacity: 0,
         duration: 0.6,
         ease: "power3.in",
+        onComplete: () => setVisible(false),
       });
     }
   }, [isOpen]);
 
-  if (!isOpen) return null;
+  // Close popup when the user presses "Escape"
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose(); // Close the popup
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+
+  if (!visible) return null;
 
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-60 backdrop-blur-md z-50">
