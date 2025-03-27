5f20a1e3166f006d32224ea88e77b359636188b4
Yasser <yasserfedsi34@gmail.com>
2025-03-27 16:49:17 +0100

    Update import path for Home component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/home";
+import Home from "./components/Home/page";
 
 export default function App() {
   return (
