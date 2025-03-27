93131d6e4070cb92c228305f84603915443d3e64
Yasser <yasserfedsi34@gmail.com>
2025-03-27 01:58:46 +0100

    Refactor main.tsx: improve code formatting and consistency

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import {StrictMode} from "react";
-import {createRoot}  from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css"
+import "./index.css";
 
-
-const root=createRoot(document.getElementById("root")!)
+const root = createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
-    <App/>
+    <App />
   </StrictMode>
 );
